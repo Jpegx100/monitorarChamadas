a7229d026433336c607939fabafe81ea56b9b714
J. Patrício <jpegx100@gmail.com>
2017-04-29 09:24:16 -0300

    Modificacao do identificador da unidade para a dsSiglaMoto

--- monitorar_db.py
+++ monitorar_db.py
@@ -17,13 +17,7 @@ def update_database(server_origin, server_destiny, time_to_update, last_date, de
 		except Exception as e:
 			print("0 Chamadas encontrados")
 		mapped_data = map_data(data, server_destiny)
-		if(len(mapped_data)<=100):
-			insert_data_in_db(mapped_data, server_destiny, default_pa, default_user)
-		else:
-			for rows in slice(mapped_data, 100):
-				insert_data_in_db(mapped_data, server_destiny, default_pa, default_user)
-				print("Slice inserted")
-		
+		insert_data_in_db(mapped_data, server_destiny, default_pa, default_user)		
 		new_last_date = max_date(mapped_data, 'dtOver')
 		save_last_date(new_last_date)
 		new_last_date = datetime.fromtimestamp(int(new_last_date) / 1000.0)
@@ -34,9 +28,149 @@ def update_database(server_origin, server_destiny, time_to_update, last_date, de
 		print("Reiniciando servico")
 		threading.Timer(time_to_update, update_database, [server_origin, server_destiny, time_to_update, last_date, default_pa, default_user]).start()
 
-def slice(l, n):
-	for i in range(0, len(l), n):
-		yield l[i:i+n]
+def get_data_from_view(last_date, server):
+	'''Connect to VIEW'''
+	try:
+		date_str = str(last_date)
+		fields = "nrTelefone, dsNomeSolicitante, dsLogradouroOrigem, dsReferenciaOrigem, "+\
+			"dsComplementoOrigem, dsBairroOrigem, nrLatOrigem, nrLngOrigem, cdChamado, "+\
+			"dtChamado, dtCancelamento, dtCadastro, dsStatus, dsPlaca, dtFinal, nrChamado, dsSiglaMoto"
+		select_query = "SELECT "+fields+" FROM [taxidigital_oaz].[dbo].[300_VwLstChamado] WHERE "
+		queryCance = select_query+"dsStatus='Cancelado' AND dtCancelamento IS NOT NULL AND CAST(dtCancelamento AS DATETIME) > '"+date_str+"'"
+		queryFinal = select_query+"dsStatus='Final' AND dtFinal IS NOT NULL AND CAST(dtFinal AS DATETIME) > '"+date_str+"'"
+		
+		conn = pymssql.connect(
+		    host=server['ip'],
+		    user=server['user'],
+		    password=server['password'],
+		    database=server['database']
+		)
+		cursor = conn.cursor(as_dict=True)
+		cursor.execute(queryCance+" UNION "+queryFinal)
+		rows = [r for r in cursor]
+		conn.close()
+
+		return rows
+	except Exception as e:
+		print(e)
+		print("\n"+queryFinal+"\n")
+
+def map_data(rows, server):
+	new_rows = []
+	j = 0
+	i = 0
+	for r in rows:
+		i = i+1
+		print("Mapped "+str(i)+" of "+str(len(rows)))
+		try:
+			new_row = {}
+			if(r['dtChamado'] and r['dtCadastro'] and r['dsSiglaMoto']):
+				hora_ch = get_hour_from_date(r['dtChamado']) if r['dtChamado'] else None
+				new_row['fone'] = r['nrTelefone'] if len(r['nrTelefone'])<=11 else r['nrTelefone'][:11]
+				new_row['nome'] = r['dsNomeSolicitante'] if len(r['dsNomeSolicitante'])<=50 else r['dsNomeSolicitante'][:50]
+				new_row['logradouro'] = r['dsLogradouroOrigem'] if len(r['dsLogradouroOrigem'])<=70 else r['dsLogradouroOrigem'][:70]
+				new_row['referencia'] = r['dsReferenciaOrigem'] if len(r['dsReferenciaOrigem'])<=60 else r['dsReferenciaOrigem'][:60]
+				new_row['dataChamada'] = r['dtChamado'].date()
+				new_row['horaChamada'] = hora_ch
+				new_row['dataSolicitacao'] = r['dtCadastro'].date()
+				new_row['horaSolicitacao'] = get_hour_from_date(r['dtCadastro']) if r['dtCadastro'] else None
+				new_row['complemento'] = r['dsComplementoOrigem'] if len(r['dsComplementoOrigem'])<=80 else r['dsComplementoOrigem'][:80]
+				new_row['bairro'] = r['dsBairroOrigem'] if len(r['dsBairroOrigem'])<=50 else r['dsBairroOrigem'][:50]
+				new_row['situacao'] = parse_Situation(r['dsStatus'])
+				new_row['latitude'] = r['nrLatOrigem']
+				new_row['longitude'] = r['nrLngOrigem']
+				new_row['idUnidade'] = r['dsSiglaMoto']
+				new_row['cdChamado'] = r['nrChamado']
+
+				if('dtCancelamento' in r.keys() and r['dtCancelamento']):
+					new_row['dataCanc'] = r['dtCancelamento'].date()
+					new_row['horaCanc'] = get_hour_from_date(r['dtCancelamento']) if r['dtCancelamento'] else None
+					new_row['dtOver'] = r['dtCancelamento']
+					new_rows.append(new_row)
+					continue
+
+				if('dtFinal' in r.keys() and r['dtFinal']):
+					new_row['dtFinal'] = r['dtFinal'].date()
+					new_row['dtOver'] = r['dtFinal']
+
+					#Insert dataCanc
+					new_row['dataCanc'] = None
+					new_row['horaCanc'] = None
+					
+					new_rows.append(new_row)
+					continue
+			else:
+				if r['dtChamado']==None:
+					print("Data do chamado não encontrada ")
+					j = j+1
+					continue
+				if r['dtCadastro']==None:
+					print("Data do cadsatro não encontrada ")
+					j = j+1
+					continue
+				if r['dsSiglaMoto']==None:
+					print("dsSiglaMoto não existe")
+					j = j+1
+					continue
+
+		except Exception as e:
+			print(str(e))
+	print("Chamadas não incluidas: "+str(j))
+	return new_rows
+
+def insert_data_in_db(rows, server, ponto_apoio_default, usuario_atend_default):
+	'''Connect to POSTGRES'''
+	conn = get_connection(server)
+	cursor = conn.cursor()
+	fields = 'id, fone, nome, logradouro, referencia, "dataChamada", "horaChamada", "dataSolicitacao",'+\
+			'"horaSolicitacao", complemento, bairro, situacao, latitude, longitude, "id_chamado_digital", "idUnidade", ' +\
+			' "idUsuarioAtend", solicitada, passagem, status, "idPontoApoio", bandeira'
+	i = 0
+	for row in rows:
+		i = i+1
+		id_chamada = str(get_next_chamada_id(server))
+		if(row["idUnidade"] and not id_chamada_duplicada(server, str(row["idUnidade"]))):
+			if(exist_unidade(server, str(row['idUnidade']))):
+				try:
+					bandeira = get_bandeira(server, row["dataChamada"], row["horaSolicitacao"][:4])
+					# In case of call been canceled
+					if row["dataCanc"]!=None:                                                
+						cursor.execute("INSERT INTO chamadas("+fields+', "dataCanc", "horaCanc"' +") VALUES ("+id_chamada+","+
+							row["fone"]+",'"+row["nome"]+"','"+row["logradouro"]+"','"+row["referencia"]+"',"+
+							row["dataChamada"].strftime("'%Y-%m-%d'")+",'"+row["horaChamada"]+"',"+
+	                                                row["dataSolicitacao"].strftime("'%Y-%m-%d'")+",'"+row["horaSolicitacao"]+"','"+
+							row["complemento"]+"','"+row["bairro"]+"','"+row["situacao"]+"',"+str(row["latitude"])+","+
+							str(row["longitude"])+", "+str(row["cdChamado"])+", "+str(row["idUnidade"])+","+
+							str(usuario_atend_default)+", 'N', 'N', 'F', "+str(ponto_apoio_default)+",'"+bandeira+"','"+str(row["dataCanc"])+"','"+
+							row["horaCanc"]+"');")
+					# In case of call been finalized
+					else:                                            
+						cursor.execute("INSERT INTO chamadas("+fields+") VALUES ("+id_chamada+","+
+							row["fone"]+",'"+row["nome"]+"','"+row["logradouro"]+"','"+row["referencia"]+"',"+
+							row["dataChamada"].strftime("'%Y-%m-%d'")+",'"+row["horaChamada"]+"',"+
+	                                                row["dataSolicitacao"].strftime("'%Y-%m-%d'")+",'"+row["horaSolicitacao"]+"','"+
+							row["complemento"]+"','"+row["bairro"]+"','"+row["situacao"]+"',"+str(row["latitude"])+","+
+							str(row["longitude"])+", "+str(row["cdChamado"])+", "+str(row["idUnidade"])+","+
+							str(usuario_atend_default)+", 'N', 'N', 'F', "+str(ponto_apoio_default)+",'"+bandeira+"');")
+				except Exception as e:
+					print(e)
+			else:
+				print("Unidade "+str(row['idUnidade'])+" não cadastrada no banco local")
+		else:
+			if row["idUnidade"]==None:
+				print("Chamada não incluida por não possuir idUnidade")
+			chamada_duplicada = get_chamada(server, str(row["cdChamado"]))
+			chamada_id = get_next_chamada_id(server)
+			# In case of call was Canceled and will be Finalized
+			if (chamada_duplicada[0][9]=='C') and (row["situacao"]=='P'):
+				cursor.execute("UPDATE chamadas SET situacao='"+row['situacao']+"' WHERE 'idUnidade'="+str(chamada_id)+");")
+			#else:
+			#	print("Chamado "+str(row['cdChamado'])+" duplicado não foi inserido.")
+		print(str(i)+" inserted of "+str(len(rows)))
+	conn.commit()
+	conn.close()
+	# max_data = max_date(rows, "dtOver")
+	# save_last_date(max_data)
 
 def max_date(rows, key):
 	bigger = 0
@@ -96,33 +230,6 @@ def load_last_date():
 		print(e)
 		return datetime.fromtimestamp(0)
 
-def get_data_from_view(last_date, server):
-	'''Connect to VIEW'''
-	try:
-		date_str = str(last_date)
-		fields = "nrTelefone, dsNomeSolicitante, dsLogradouroOrigem, dsReferenciaOrigem, "+\
-			"dsComplementoOrigem, dsBairroOrigem, nrLatOrigem, nrLngOrigem, cdChamado, "+\
-			"dtChamado, dtCancelamento, dtCadastro, dsStatus, dsPlaca, dtFinal, nrChamado"
-		select_query = "SELECT TOP 150 "+fields+" FROM [taxidigital_oaz].[dbo].[300_VwLstChamado] WHERE "
-		queryCance = select_query+"dsStatus='Cancelado' AND dtCancelamento IS NOT NULL AND CAST(dtCancelamento AS DATETIME) > '"+date_str+"'"
-		queryFinal = select_query+"dsStatus='Final' AND dtFinal IS NOT NULL AND CAST(dtFinal AS DATETIME) > '"+date_str+"'"
-		
-		conn = pymssql.connect(
-		    host=server['ip'],
-		    user=server['user'],
-		    password=server['password'],
-		    database=server['database']
-		)
-		cursor = conn.cursor(as_dict=True)
-		cursor.execute(queryCance+" UNION "+queryFinal)
-		rows = [r for r in cursor]
-		conn.close()
-
-		return rows
-	except Exception as e:
-		print(e)
-		print("\n"+queryFinal+"\n")
-
 def get_connection(server):
 	conn_string = "host="+server['ip']+" dbname="+server['database']+" user="+server['user']+" password="+server['password']
 	conn = psycopg2.connect(conn_string)
@@ -140,55 +247,6 @@ def get_next_chamada_id(server):
 	except Exception as e:
 		print(e)
 	return None
-
-def insert_data_in_db(rows, server, ponto_apoio_default, usuario_atend_default):
-	'''Connect to POSTGRES'''
-	conn = get_connection(server)
-	cursor = conn.cursor()
-	fields = 'id, fone, nome, logradouro, referencia, "dataChamada", "horaChamada", "dataSolicitacao",'+\
-			'"horaSolicitacao", complemento, bairro, situacao, latitude, longitude, "id_chamado_digital", "idUnidade", ' +\
-			' "idUsuarioAtend", solicitada, passagem, status, "idPontoApoio", bandeira'
-	i = 0
-	for row in rows:
-		i = i+1
-		if(not id_chamada_duplicada(server, str(row["cdChamado"]))):
-			try:
-				chamada_id = str(get_next_chamada_id(server))
-				bandeira = get_bandeira(server, row["dataChamada"], row["horaSolicitacao"][:4])
-				# In case of call been canceled
-				if row["dataCanc"]!=None:                                                
-					cursor.execute("INSERT INTO chamadas("+fields+', "dataCanc", "horaCanc"' +") VALUES ("+chamada_id+","+
-						row["fone"]+",'"+row["nome"]+"','"+row["logradouro"]+"','"+row["referencia"]+"',"+
-						row["dataChamada"].strftime("'%Y-%m-%d'")+",'"+row["horaChamada"]+"',"+
-                                                row["dataSolicitacao"].strftime("'%Y-%m-%d'")+",'"+row["horaSolicitacao"]+"','"+
-						row["complemento"]+"','"+row["bairro"]+"','"+row["situacao"]+"',"+str(row["latitude"])+","+
-						str(row["longitude"])+", "+str(row["cdChamado"])+", "+str(row["idUnidade"])+","+
-						str(usuario_atend_default)+", 'N', 'N', 'F', "+str(ponto_apoio_default)+",'"+bandeira+"','"+str(row["dataCanc"])+"','"+
-						row["horaCanc"]+"');")
-				# In case of call been finalized
-				else:                                            
-					cursor.execute("INSERT INTO chamadas("+fields+") VALUES ("+chamada_id+","+
-						row["fone"]+",'"+row["nome"]+"','"+row["logradouro"]+"','"+row["referencia"]+"',"+
-						row["dataChamada"].strftime("'%Y-%m-%d'")+",'"+row["horaChamada"]+"',"+
-                                                row["dataSolicitacao"].strftime("'%Y-%m-%d'")+",'"+row["horaSolicitacao"]+"','"+
-						row["complemento"]+"','"+row["bairro"]+"','"+row["situacao"]+"',"+str(row["latitude"])+","+
-						str(row["longitude"])+", "+str(row["cdChamado"])+", "+str(row["idUnidade"])+","+
-						str(usuario_atend_default)+", 'N', 'N', 'F', "+str(ponto_apoio_default)+",'"+bandeira+"');")
-			except Exception as e:
-				print(e)
-		else:
-			chamada_duplicada = get_chamada(server, str(row["cdChamado"]))
-			chamada_id = str(get_next_chamada_id(server))
-			# In case of call was Canceled and will be Finalized
-			if (chamada_duplicada[0][9]=='C') and (row["situacao"]=='P'):
-				cursor.execute("UPDATE chamadas SET situacao='"+row["situacao"]+' WHERE "idUnidade"='+str(row["idUnidade"])+");")
-			#else:
-			#	print("Chamado "+str(row['cdChamado'])+" duplicado não foi inserido.")
-		print(str(i)+" inserted of "+str(len(rows)))
-	conn.commit()
-	conn.close()
-	# max_data = max_date(rows, "dtOver")
-	# save_last_date(max_data)
 
 def get_hour_from_date(date):
 	hour = str(date.hour) if len(str(date.hour))>1 else "0"+str(date.hour)
@@ -216,54 +274,18 @@ def parse_Situation(situation):
 	if situation.lower() == 'final': return 'P'
 	if situation.lower() == 'cancelado': return 'C'
 
-def map_data(rows, server):
-	new_rows = []
-	i = 0
-	for r in rows:
-		i = i+1
-		print("Mapped "+str(i)+" of "+str(len(rows)))
-		try:
-			new_row = {}
-			num_unidade = get_id_unidade(r['dsPlaca'], server)
-			if(num_unidade!=None and r['dtChamado'] and r['dtCadastro']):
-				hora_ch = get_hour_from_date(r['dtChamado']) if r['dtChamado'] else None
-				new_row['fone'] = r['nrTelefone'] if len(r['nrTelefone'])<=11 else r['nrTelefone'][:11]
-				new_row['nome'] = r['dsNomeSolicitante'] if len(r['dsNomeSolicitante'])<=50 else r['dsNomeSolicitante'][:50]
-				new_row['logradouro'] = r['dsLogradouroOrigem'] if len(r['dsLogradouroOrigem'])<=70 else r['dsLogradouroOrigem'][:70]
-				new_row['referencia'] = r['dsReferenciaOrigem'] if len(r['dsReferenciaOrigem'])<=60 else r['dsReferenciaOrigem'][:60]
-				new_row['dataChamada'] = r['dtChamado'].date()
-				new_row['horaChamada'] = hora_ch
-				new_row['dataSolicitacao'] = r['dtCadastro'].date()
-				new_row['horaSolicitacao'] = get_hour_from_date(r['dtCadastro']) if r['dtCadastro'] else None
-				new_row['complemento'] = r['dsComplementoOrigem'] if len(r['dsComplementoOrigem'])<=80 else r['dsComplementoOrigem'][:80]
-				new_row['bairro'] = r['dsBairroOrigem'] if len(r['dsBairroOrigem'])<=50 else r['dsBairroOrigem'][:50]
-				new_row['situacao'] = parse_Situation(r['dsStatus'])
-				new_row['latitude'] = r['nrLatOrigem']
-				new_row['longitude'] = r['nrLngOrigem']
-				new_row['idUnidade'] = num_unidade
-				new_row['cdChamado'] = r['nrChamado']
-
-				if('dtCancelamento' in r.keys() and r['dtCancelamento']):
-					new_row['dataCanc'] = r['dtCancelamento'].date()
-					new_row['horaCanc'] = get_hour_from_date(r['dtCancelamento']) if r['dtCancelamento'] else None
-					new_row['dtOver'] = r['dtCancelamento']
-					new_rows.append(new_row)
-					continue
-
-				if('dtFinal' in r.keys() and r['dtFinal']):
-					new_row['dtFinal'] = r['dtFinal'].date()
-					new_row['dtOver'] = r['dtFinal']
-
-					#Insert dataCanc
-					new_row['dataCanc'] = None
-					new_row['horaCanc'] = None
-					
-					new_rows.append(new_row)
-					continue
-
-		except Exception as e:
-			print(str(e))
-	return new_rows
+def exist_unidade(server, id_unidade):
+	try:
+		conn = get_connection(server)
+		cursor = conn.cursor()
+		cursor.execute("SELECT id from unidades where id="+id_unidade+";")
+		row = [r for r in cursor]
+		conn.close()
+		if len(row)>0:
+			return True
+	except Exception as e:
+		print(e)
+	return False
 
 def id_chamada_duplicada(server, taxi_digital_id):
 	try:
